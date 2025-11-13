54bfca3d1c807c609603e466675cef19528c9f6a
Vadim Evstegneev <ev.vadim@gmail.com>
2025-11-13 18:43:02 +0300

    add delete endpoint

--- src/test/java/config/endpoints/Endpoints.java
+++ src/test/java/config/endpoints/Endpoints.java
@@ -3,6 +3,7 @@ package config.endpoints;
 public class Endpoints {
 
     public static final String CREATE_USER = "/api/auth/register";
+    public static final String DELETE_USER = "/api/auth/user";
     public static final String LOGIN_USER = "/api/auth/login";
 
 }
