d7d86a0ed1e4b5ae0196a24308ec4b336fae58e8
Vadim Evstegneev <ev.vadim@gmail.com>
2025-11-14 21:34:53 +0300

    implement token processing

--- src/test/java/data/user/delete/request/DeleteUserRequest.java
+++ src/test/java/data/user/delete/request/DeleteUserRequest.java
@@ -11,7 +11,7 @@ import static io.restassured.RestAssured.given;
 
 public class DeleteUserRequest extends FetchResponse {
 
-    private final String accessToken;
+    private String accessToken;
 
     public DeleteUserRequest(String accessToken) {
         this.accessToken = accessToken;
@@ -19,9 +19,13 @@ public class DeleteUserRequest extends FetchResponse {
 
     @Step("Delete User DELETE Request")
     protected Response requestAndCheckResponseSpec(ResponseSpecification specification) {
+
+        String prefix = "Bearer ";
+        accessToken = accessToken.startsWith(prefix) ? accessToken.substring(prefix.length()) : accessToken;
+
         Response response = given()
                 .spec(CommonDeleteUserRequest.requestSpecification)
-                .auth().oauth2(accessToken.substring(7))
+                .auth().oauth2(accessToken)
                 .delete(Endpoints.DELETE_USER);
 
         if (specification != null) {
